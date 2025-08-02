e2d085ee84d68478dad3abf481fec8b6493b06b8
aayush598 <aayushgid598@gmail.com>
2025-08-02 16:19:37 +0530

    host and port updated

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
+
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP(name="MathServer", stateless_http=True)
+mcp = FastMCP("web-search", host="0.0.0.0", port=8000)
 
 @mcp.tool(description="A simple add tool")
 def add(a: int, b: int) -> int:
     return a + b
 
 if __name__ == "__main__":
-    mcp.run(transport='sse', host="0.0.0.0", port=8000)
+    mcp.run(transport="sse")
